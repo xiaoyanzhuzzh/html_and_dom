fe7628e795f4d94a064331d16668948712103144
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2014-08-11 09:21:08 +0800

    changed

--- main.js
+++ main.js
@@ -1,116 +1,102 @@
-
 function Get_scores() {
-  var score1 = Get_Score1();
-
-  var score2_1 = Get_Score2_1();
-  var score2_2 = Get_Score2_2();
-
-  var score3_1 = Get_Score3_1();
-  var score3_2 = Get_Score3_2();
-
-  var score4_1 = Get_Score4_1();
-  var score4_2 = Get_Score4_2();
-
-
-  var score5 = Get_Score5();
-
   var scores = 0;
-  scores = score1 + score2_1 +score2_2 + score3_1 +score3_2 + score4_1 + score4_2 + score5;
-
+  scores =  fullInTopics() + choiceTopics() + multipleChoiceTopics()
+            + trueOrFalseTopics() + shortAnswerTopics();
   document.getElementById("scores").value = scores;
   return false;
-
 }
 
-function Get_Score1() {
-  var score1_1 = 0;
-  var value1_1_1 = document.getElementById("1_1_1").value;
-  if (value1_1_1 == "统一建模语言"){
-      score1_1 += 5;
+function  fullInTopics(){
+  var fullInSubject1 = new Subject('fullInSubject',['统一建模语言'],1,5);
+  var fullInSubject2 = new Subject('fullInSubject',['继承性','多态性','封装性'],3,5);
+
+  var value1_1_1 = document.getElementById('1_1_1').value;
+  if(value1_1_1 == fullInSubject1.answer[0]){
+    fullInSubject1.scores += fullInSubject1.scorePerSubject;
   }
 
-  var score1_2 = 0;
-  var answer = ["继承性","多态性","封装性"];
-  var value = [];
-  value.push(document.getElementById("1_2_1").value);
-  value.push(document.getElementById("1_2_2").value);
-  value.push(document.getElementById("1_2_3").value);
+  var value1_2 = [];
+  value1_2.push(document.getElementById('1_2_1').value);
+  value1_2.push(document.getElementById('1_2_2').value);
+  value1_2.push(document.getElementById('1_2_3').value);
 
-  for (var i = 0; i < answer.length; i++){
-    for (var j = 0;j < value.length; j++) {
-      if(answer[i] == value[j]){
-        score1_2 += 5;
+  for(var i = 0; i < fullInSubject2 .answer.length; i++){
+    for(var j = 0;j < value1_2.length; j++){
+      if(fullInSubject2.answer[i] == value1_2[j]){
+        fullInSubject2.scores +=  fullInSubject2.scorePerSubject;
         break;
       }
     }
   }
-  return score1_1 + score1_2;
+  return fullInSubject1.scores + fullInSubject2.scores;
 }
 
-function Get_Score2_1() {
-  var score2_1 = 0;
-  if(document.getElementById("2_1_2").checked){
-    score2_1 +=10;
+function choiceTopics(){
+  var choiceSubject = new Subject('choiceSubject',['B','A'],2,10);
+
+  var choiceSubject1 = new ChoiceSubject('radio_ans_1');
+  var value1 = choiceSubject1.calculation();
+  var choiceSubject2 = new ChoiceSubject('radio_ans_2');
+  var value2 = choiceSubject2.calculation();
+  var value = [value1,value2];
+
+  for(var i = 0; i < value.length; i++){
+    if(value[i] == choiceSubject.answer[i]){
+      choiceSubject.scores += choiceSubject.scorePerSubject;
+    }
   }
-  return score2_1;
+  return choiceSubject.scores;
 }
 
-function Get_Score2_2() {
-  var score2_2 = 0;
-  if(document.getElementById("2_1_2").checked){
-    score2_2 +=10;
+function multipleChoiceTopics(){
+  var multipleChoiceSubject = new Subject('multipleChoiceSubject',
+  [['A','B','D'],['A','B','C']],2,10);
+
+  var multipleChoiceSubject1 = new MultipleChoiceSubject('check_ans_1');
+  var value1 = multipleChoiceSubject1.calculation();
+  var answer1 = multipleChoiceSubject.answer[0];
+  if(answer1.length == value1.length){
+    var diffA = _.difference(value1,answer1);
+    if(_.isEmpty(diffA)){
+        multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
+    }
   }
-  return score2_2;
-}
 
-function Get_Score3_1() {
-  var score3_1 = 0;
-  if(!document.getElementById("3_1_3").checked){
-    var yes1 = document.getElementById("3_1_1").checked;
-    var yes2 = document.getElementById("3_1_2").checked;
-    var yes3 = document.getElementById("3_1_4").checked;
-    if(yes1 && yes2 && yes3){
-         score3_1 += 10;
-      }
+  var multipleChoiceSubject2 = new MultipleChoiceSubject('check_ans_2');
+  var value2 = multipleChoiceSubject2.calculation();
+  var answer2 = multipleChoiceSubject.answer[1];
+  if(answer2.length == value2.length){
+    var diffB = _.difference(value2,answer2);
+    if(_.isEmpty(diffB)){
+          multipleChoiceSubject.scores += multipleChoiceSubject.scorePerSubject;
+    }
   }
-  return score3_1;
+  return multipleChoiceSubject.scores;
 }
 
-function Get_Score3_2() {
-  var score3_2 = 0;
-  if(!document.getElementById("3_2_4").checked){
-    var yes1 = document.getElementById("3_2_1").checked;
-    var yes2 = document.getElementById("3_2_2").checked;
-    var yes3 = document.getElementById("3_2_3").checked;
-    if(yes1 && yes2 && yes3){
-         score3_2 += 10;
-      }
+function trueOrFalseTopics(){
+  var trueOrFalseSubject = new Subject('trueOrFalseSubject',['no','yes'],2,10);
+  var trueOrFalseSubject1 =new ChoiceSubject('ans_1');
+  var value1 = trueOrFalseSubject1.calculation();
+  var trueOrFalseSubject2 =new ChoiceSubject('ans_2');
+  var value2 = trueOrFalseSubject2.calculation();
+  var value = [value1,value2];
+
+  for(var i = 0; i < value.length; i++){
+    if(value[i] ==  trueOrFalseSubject.answer[i]){
+       trueOrFalseSubject.scores +=  trueOrFalseSubject.scorePerSubject;
+    }
   }
-  return score3_2;
+  return trueOrFalseSubject.scores;
 }
 
-function Get_Score4_1() {
-  var score4_1 = 0;
-  if(document.getElementById("4_1_2").checked){
-    score4_1 +=10;
-  }
-  return score4_1;
-}
-
-function Get_Score4_2() {
-  var score4_2 = 0;
-  if(document.getElementById("4_2_1").checked){
-    score4_2 +=10;
-  }
-  return score4_2;
-}
-
-function Get_Score5() {
-  var score5 = 0;
+function shortAnswerTopics(){
+  var shortAnswerSubject = new Subject(shortAnswerSubject,
+    ['模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。'],
+    1,20);
   var value5 = document.getElementById("5").value;
-   if (value5 == "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。"){
-       score5 += 20;
-     }
-
-       return score5;
- }
+  if( value5 == shortAnswerSubject.answer[0]){
+    shortAnswerSubject.scores = shortAnswerSubject.scorePerSubject;
+  }
+  return shortAnswerSubject.scores;
+}
